de38a84d26a47d15b93247732e532688f2eeb360
saro92 <sada.roth@gmail.com>
2025-01-31 17:57:17 +0100

    Removed inspect module and changed costum_log

--- utils/logs/create_logger.py
+++ utils/logs/create_logger.py
@@ -1,12 +1,10 @@
 import logging
-import inspect
 
 class MakeLogger:
     '''creating logger instance'''
 
     def costum_log(self, loglevel = logging.DEBUG, filename="log.log", modification="w"):
-        logger_name = inspect.stack()[1][3]
-        logger=logging.getLogger(logger_name)
+        logger=logging.getLogger(filename)
         logger.setLevel(loglevel)
         handler = logging.FileHandler(filename, mode=modification)
         formatter = logging.Formatter("%(asctime)s -%(name)s - %(levelname)s - %(message)s")
