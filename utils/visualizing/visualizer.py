d5af33d4c216136946d9d162b60c8e85cfddc20b
saro92 <sada.roth@gmail.com>
2025-01-31 23:27:55 +0100

    changed __init__ input

--- utils/visualizing/visualizer.py
+++ utils/visualizing/visualizer.py
@@ -1,7 +1,12 @@
 class Visualizer:
     '''Visualizer feeded preprocessed data using multiple functions to display.'''
-    def __init__(self, dataframe):
-        pass
+    def __init__(self, f_df, t_df):
+        """
+        :param f_df: feature pandas data frame, holding all the features to be analyzed
+        :param t_df: target pandas data frame, holding only the target values
+        """
+        self.f_df = f_df
+        self.t_df = t_df
 
     def descirbe(self):
         pass
@@ -9,5 +14,5 @@ class Visualizer:
     def __str__(self):
         pass
 
-    def visualize(self):
+    def boxplot(self):
         pass
