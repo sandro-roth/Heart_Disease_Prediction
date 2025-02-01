9716b4177697ab140275e585357565191268ef7c
saro92 <sada.roth@gmail.com>
2025-02-01 16:52:07 +0100

    Added correlation plot and save method

--- utils/visualizing/visualizer.py
+++ utils/visualizing/visualizer.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,24 +14,46 @@ class Visualizer:
         self.f_df = f_df
         self.t_df = t_df
         self.data = pd.concat([f_df, t_df], axis=1)
+        self.pic_name = None
 
 
     def __str__(self):
         """Print statement returns properties of data like the shape and the feature names"""
-        return f'The current dataset has the following properties:\n' \
-               f'shape: {self.data.shape}\n' \
-               f'feature-names: {self.data.columns.to_list()}'
+        return f'\nThe current dataset has the following properties:\n' \
+               f'- shape: {self.data.shape}\n' \
+               f'- feature-names: {self.data.columns.to_list()}'
+
 
     def pairplot(self):
         """Returns a sns pairplot of all numeric features of the data."""
+        self.pic_name = 'Feature_pairplot.png'
         plt.clf()
-        sns.pairplot(self.data, hue='num', hue_order=[0,1], markers=['X', 'o'])
-        plt.show()
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16,9))
+        sns.pairplot(self.data, hue='target', hue_order=[0,1], markers=['X', 'o'])
+        ax.set_title('Pairplot of numeric features')
+        # Instead of show save the image
+        #plt.show()
 
 
     def correlation(self):
-        pass
+        """Creates linear correlation plot in a heatmap"""
+        self.pic_name = 'Feature_correlation.png'
+        plt.clf()
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16,9))
+        correlation = self.data.corr()
+        sns.heatmap(correlation, annot=True, fmt='.2f', linewidth=.5)
+        ax.set_title('Correlation map of features')
+        ax.title.set_size(20)
+        # Instead of show save the image
+        #plt.show()
 
 
     def boxplot(self):
         pass
+
+
+    def save_pic(self, path_to_file):
+        """In order to save the current picture this function is called"""
+        if self.pic_name:
+            plt.savefig(os.path.join(path_to_file,self.pic_name))
+            self.pic_name = None
