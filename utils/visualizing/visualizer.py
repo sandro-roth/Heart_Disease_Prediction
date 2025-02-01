7c982b8516860aaa5c2d22175cc502fb0996dea4
saro92 <sada.roth@gmail.com>
2025-02-01 20:27:57 +0100

    Added barplot for multiple features

--- utils/visualizing/visualizer.py
+++ utils/visualizing/visualizer.py
@@ -11,9 +11,18 @@ class Visualizer:
         :param f_df: feature pandas data frame, holding all the features to be analyzed
         :param t_df: target pandas data frame, holding only the target values
         """
+
         self.f_df = f_df
         self.t_df = t_df
         self.data = pd.concat([f_df, t_df], axis=1)
+
+        # Create column with correct target names
+        self.data['target_name'] = self.data['target'].map(str)
+        self.data['target_name'] = self.data['target_name'].str.replace('0', 'Normal')
+        self.data['target_name'] = self.data['target_name'].str.replace('1', 'Diseased')
+        self.data['target_name'] = self.data['target_name'].astype('category')
+
+        self.hue = 'target_name'
         self.pic_name = None
 
 
@@ -27,29 +36,44 @@ class Visualizer:
     def pairplot(self):
         """Returns a sns pairplot of all numeric features of the data."""
         self.pic_name = 'Feature_pairplot.png'
-        plt.clf()
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16,9))
-        sns.pairplot(self.data, hue='target', hue_order=[0,1], markers=['X', 'o'])
+        sns.pairplot(self.data, hue=self.hue, hue_order=[0,1], markers=['X', 'o'])
         ax.set_title('Pairplot of numeric features')
-        # Instead of show save the image
-        #plt.show()
+        ax.title.set_size(20)
 
 
     def correlation(self):
         """Creates linear correlation plot in a heatmap"""
         self.pic_name = 'Feature_correlation.png'
-        plt.clf()
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16,9))
         correlation = self.data.corr()
         sns.heatmap(correlation, annot=True, fmt='.2f', linewidth=.5)
         ax.set_title('Correlation map of features')
         ax.title.set_size(20)
-        # Instead of show save the image
-        #plt.show()
+
+
+    def barplot(self, f_name):
+        """
+        Creates a barplot of the different values of the feature
+        :param f_name: str, feature name which will be visualized
+        """
+        lookup = {'sex': {'labels': ['Female', 'Male'], 'title': 'Diseased Female vs Male'},
+                  'fbs': {'labels': ['False', 'True'], 'title': 'Fasting Blood sugar'},
+                  'exang': {'labels': ['No', 'Yes'], 'title': 'Exercise induced Angina'},
+                  'cp': {'labels': ['Typical angina', 'Atypical angina', 'Non-Aningal pain', 'Asymptomatic'], 'title': 'Chest Pain Types'}}
+        self.pic_name = f_name + '_countplot.png'
+
+        g = sns.catplot(self.data, x=f_name, hue=self.hue, kind='count', errorbar='ci', height=5, aspect=1.5, legend=False)
+        g.set_xticklabels(lookup[f_name]['labels'])
+        g.fig.subplots_adjust(top=0.9)
+        g.fig.suptitle(lookup[f_name]['title'])
+        plt.legend(title='Target', loc='upper left', labels=self.data['target_name'])
 
 
     def boxplot(self):
-        pass
+        """Create boxplots of categorical features"""
+        
+
 
 
     def save_pic(self, path_to_file):
